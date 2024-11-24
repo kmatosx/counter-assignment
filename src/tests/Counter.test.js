1ecff5540058e31902e1239708932e47819c09dc
Kathy Matos <kmatos@ufl.edu>
2024-11-24 18:51:33 -0500

    submit

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,45 @@
 // import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
 
-beforeEach(() => {
-  // Render the Counter component here
-})
+// import the Counter component here
+import Counter from '../components/Counter';
+
+// beforeEach(() => {
+//    render(<Counter />);
+// })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter initialCount={0}/>);
+  const countValue = screen.getByTestId('count');
+  expect(countValue).toHaveTextContent('0');
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', () => { 
+  render(<Counter initialCount={0}/>);
+  // Find the increment button const increment
+  const incrementButton = screen.getByText('+'); 
+  // Click the increment button 
+  fireEvent.click(incrementButton); 
+  // Verify that the count has increased by 1 
+  const countValue = screen.getByTestId('count'); 
+  expect(countValue).toHaveTextContent('1'); 
+}); 
+  
+test('clicking - decrements the count', () => { 
+  render(<Counter initialCount={0}/>);
+  // Find the decrement button 
+  const decrementButton = screen.getByText('-'); 
+  // Click the decrement button 
+  fireEvent.click(decrementButton); 
+  // Verify that the count has decreased by 1
+  const countValue = screen.getByTestId('count'); 
+  expect(countValue).toHaveTextContent('-1'); 
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
-});
+
